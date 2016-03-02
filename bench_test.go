7a1df2af545d653c6e01b11220835896fcd5e124
Damian Gryski <dgryski@gmail.com>
2016-03-02 22:32:46 +0100

    more benchmarks

--- bench_test.go
+++ bench_test.go
@@ -20,7 +20,9 @@ func benchmarkLookup(b *testing.B, nbuckets int) {
 	}
 }
 
-func BenchmarkLookup8(b *testing.B)   { benchmarkLookup(b, 8) }
-func BenchmarkLookup32(b *testing.B)  { benchmarkLookup(b, 32) }
-func BenchmarkLookup128(b *testing.B) { benchmarkLookup(b, 128) }
-func BenchmarkLookup512(b *testing.B) { benchmarkLookup(b, 512) }
+func BenchmarkLookup8(b *testing.B)    { benchmarkLookup(b, 8) }
+func BenchmarkLookup32(b *testing.B)   { benchmarkLookup(b, 32) }
+func BenchmarkLookup128(b *testing.B)  { benchmarkLookup(b, 128) }
+func BenchmarkLookup512(b *testing.B)  { benchmarkLookup(b, 512) }
+func BenchmarkLookup2048(b *testing.B) { benchmarkLookup(b, 2048) }
+func BenchmarkLookup8192(b *testing.B) { benchmarkLookup(b, 8192) }
